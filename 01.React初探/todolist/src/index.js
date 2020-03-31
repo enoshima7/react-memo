245bf774ef186b8b33c3916b36cbcb8873755e3b
enoshima7 <duanjipeng0328@gmail.com>
2020-03-31 21:57:13 +0800

    Day 1

---
+++ 01.React初探/todolist/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// JSX语法
+/*
+  使用自己创建的组件 <App />
+*/
+ReactDOM.render(<App />, document.getElementById('root'));
