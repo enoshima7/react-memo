7c2dbb6205cdc03123eb745438ae2f63478a1778
enoshima7 <duanjipeng0328@gmail.com>
2020-04-05 00:30:07 +0800

    Day 6 头部组件

--- 04.项目实战/jianshu/Memo.md
+++ 04.项目实战/jianshu/Memo.md
@@ -2,3 +2,8 @@
 1. 安装 styled-components 管理css文件 scoped
     - 把css变成js
 2. 使用reset.css globalstyle
+3. styled-components的用法
+    - attrs
+    - &.xxx
+    - &::placeholder
+    - url引入图片用import加${}语法
--- 04.项目实战/jianshu/src/common/header/index.js
+++ 04.项目实战/jianshu/src/common/header/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import HeaderWrapper from './style';
+import { HeaderWrapper, Logo,Nav ,NavItem,NavSearch,Addition,Btn} from './style';
 class Header extends Component {
   render() {
-    return <HeaderWrapper>Header</HeaderWrapper>;
+    return (
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavSearch></NavSearch>
+        </Nav>
+        <Addition>
+          <Btn className='writting'>写文章</Btn>
+          <Btn className='reg'>注册</Btn>
+        </Addition>
+      </HeaderWrapper>
+    );
   }
 }
 export default Header;
